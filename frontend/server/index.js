d89e785b31d27023bbf1bda38220c341ae3c5351
RemilekunElijah <remilekunelijah21997@gmail.com>
2023-01-13 17:29:16 +0100

    merge

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -6,12 +6,12 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/ping", function(req, res) {
-    return res.send("pong");
+app.get("/ping", function (req, res) {
+ return res.send("pong");
 });
 
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../build/index.html"));
+app.get("/*", function (req, res) {
+ res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
-app.listen(port, () => console.log(`app is running on port ${port}`));
+app.listen(port, _ => console.log("Server started on port", port));
