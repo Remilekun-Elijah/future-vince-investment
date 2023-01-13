8368c9c7405858b92b3e6758ce0ace53aa51055f
RemilekunElijah <remilekunelijah21997@gmail.com>
2023-01-13 17:08:08 +0100

    updated server file

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("path");
-const app = express()
-const port = process.env.PORT||9000
+const port = process.env.PORT || 3002;
+const app = express();
 
-app.use(express.static(path.join(__dirname, "../build")))
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/ping", (req, res)=> res.send("Pong!"))
+app.get("/ping", function(req, res) {
+    return res.send("pong");
+});
 
-app.get("/*", (req, res) => {
- const file = path.join(__dirname, "../build", "index.html");
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
- console.log("hello", file)
- res.sendFile(file)
-})
-
-app.listen(port, _=> console.log("Server started on port", port))
+app.listen(port, () => console.log(`app is running on port ${port}`));
