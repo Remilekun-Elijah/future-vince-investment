ef7db3fa357976f79269b942a40fef6954928958
= <=>
2024-01-25 14:29:59 +0100

    removes unwanted package

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,10 @@
 // jshint esversion:8
 "use strict";
 
-const { Server } = require("socket.io");
 const { createServer } = require("http");
-const fs = require("fs");
+// const fs = require("fs");
 const config = require("./configs/config");
-const path = require("path");
+// const path = require("path");
 const database = require("./configs/database");
 
 global.logger = require("./logger");
@@ -14,59 +13,12 @@ const app = require("./configs/express");
 
 logger.info("Logger is ready");
 
-const options = {
-  key: fs.readFileSync(path.resolve(config.server_key)),
-  cert: fs.readFileSync(path.resolve(config.server_cert)),
-};
+// const options = {
+//   key: fs.readFileSync(path.resolve(config.server_key)),
+//   cert: fs.readFileSync(path.resolve(config.server_cert)),
+// };
 // create express app using http server
-const server = createServer(options, app);
-
-/** Create socket connection */
-
-global.io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-  serveClient: false,
-  // transports: ['polling', 'websocket'],
-  // allowEIO3: true,
-});
-
-let users = [];
-
-global.io.engine.on("connection_error", (err) => {
-  logger.error("socket_connection_error", err);
-});
-
-global.io.on("connection", (client) => {
-  logger.info("connected", client.id);
-  // event fired when the chat room is disconnected
-  client.on("disconnect", () => {
-    users = users.filter((user) => user.socketId !== client.id);
-    logger.warn("disconnect", users);
-  });
-
-  // add identity of user mapped to the socket id
-  client.on("identity", (userId) => {
-    users.push({
-      socketId: client.id,
-      userId: userId,
-    });
-
-    logger.info("identity", users);
-  });
-
-  // subscribe person to chat & other user as well
-  client.on("subscribe", (roomId, otherUserId = "") => {
-    client.join(roomId);
-    logger.info("subscribe", [roomId, otherUserId, users]);
-  });
-
-  // mute a chat room
-  client.on("unsubscribe", (roomId) => {
-    client.leave(roomId);
-  });
-});
+const server = createServer({}, app);
 
 const port = app.get("port");
 const startup = () => {
